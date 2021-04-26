e086ccda44a43e43b7f78fcaf4d3143dc40e6f06
Luminaes <agustin.luvini@gmail.com>
2021-04-26 18:27:46 -0300

    Desing

---
+++ app/src/main/java/com/example/ddassistant/data/Record.kt
@@ -0,0 +1,12 @@
+package com.example.ddassistant.data
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ddassistant.R
+
+class Record : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record)
+    }
+}
