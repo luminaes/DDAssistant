87ac9b055e35331da5342d17235ef2d7c2e318b0
Luminaes <agustin.luvini@gmail.com>
2021-05-20 00:05:32 -0300

    media player

--- app/src/main/java/com/example/ddassistant/AudioPlayer.kt
+++ app/src/main/java/com/example/ddassistant/AudioPlayer.kt
@@ -4,7 +4,9 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.SeekBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -13,29 +15,35 @@ import java.util.Collections.max
 
 class AudioPlayer : AppCompatActivity() {
     private var mp: MediaPlayer? =null
-    private var currentSong = mutableListOf(R.raw.a)
+    private var currentSong = mutableListOf(R.raw.push)
+    private lateinit var seekBar: SeekBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audio_player)
-
-        val playButton  =findViewById<FloatingActionButton>(R.id.flo_btn_activity_audio_player_play)
+        /*val playButton  =findViewById<FloatingActionButton>(R.id.flo_btn_activity_audio_player_play)
         val pauseButton  =findViewById<FloatingActionButton>(R.id.flo_btn_activity_audio_player_play)
         val stopButton  =findViewById<FloatingActionButton>(R.id.flo_btn_activity_audio_player_play)
         playButton.scaleType = ImageView.ScaleType.CENTER
         pauseButton.scaleType = ImageView.ScaleType.CENTER
-        stopButton.setScaleType(ImageView.ScaleType.CENTER)
-        controlSound(currentSong[0])
+        stopButton.setScaleType(ImageView.ScaleType.CENTER)*/
+        //controlSound(currentSong[0])
     }
-    private fun controlSound (id:Int){
+
+    override fun onStart() {
+        super.onStart()
         val playButton  =findViewById<FloatingActionButton>(R.id.flo_btn_activity_audio_player_play)
-        val pauseButton  =findViewById<FloatingActionButton>(R.id.flo_btn_activity_audio_player_play)
-        val stopButton  =findViewById<FloatingActionButton>(R.id.flo_btn_activity_audio_player_play)
+        val pauseButton  =findViewById<FloatingActionButton>(R.id.flo_btn_activity_audio_player_pause)
+        val stopButton  =findViewById<FloatingActionButton>(R.id.flo_btn_activity_audio_player_stop)
+        playButton.scaleType = ImageView.ScaleType.CENTER
+        pauseButton.scaleType = ImageView.ScaleType.CENTER
+        stopButton.setScaleType(ImageView.ScaleType.CENTER)
         val seekbar = findViewById<SeekBar>(R.id.ske_activity_audio_player)
+        val id = currentSong[0]
         playButton.setOnClickListener{
             if (mp== null ){
                 mp= MediaPlayer.create(this,id)
                 Log.d("AudioPlayer","ID: ${mp!!.audioSessionId} seconds")
-                initialiseSeekBar()
+                //initialiseSeekBar()
             }
             mp?.start()
             Log.d("AudioPlayer","Duration: ${mp!!.duration/1000} seconds")
@@ -54,7 +62,42 @@ class AudioPlayer : AppCompatActivity() {
                 mp = null
             }
         }
-        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+
+
+    }
+
+
+
+    private fun controlSound (id:Int){
+        val playButton  =findViewById<FloatingActionButton>(R.id.flo_btn_activity_audio_player_play)
+        val pauseButton  =findViewById<FloatingActionButton>(R.id.flo_btn_activity_audio_player_play)
+        val stopButton  =findViewById<FloatingActionButton>(R.id.flo_btn_activity_audio_player_play)
+        val seekbar = findViewById<SeekBar>(R.id.ske_activity_audio_player)
+        playButton.setOnClickListener{
+            if (mp== null ){
+                mp= MediaPlayer.create(this,id)
+                Log.d("AudioPlayer","ID: ${mp!!.audioSessionId} seconds")
+                //initialiseSeekBar()
+            }
+            mp?.start()
+            Log.d("AudioPlayer","Duration: ${mp!!.duration/1000} seconds")
+        }
+
+        pauseButton.setOnClickListener{
+            if(mp !==null) mp?.pause()
+            Log.d("AudioPlayer","Pause at: ${mp!!.currentPosition/1000} seconds")
+
+        }
+        stopButton.setOnClickListener{
+            if(mp !==null){
+                mp?.stop()
+                mp?.reset()
+                mp?.release()
+                mp = null
+            }
+        }
+
+   /*     seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 if (fromUser) mp?.seekTo(progress)
             }
@@ -64,14 +107,17 @@ class AudioPlayer : AppCompatActivity() {
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
             }
-        })
+
+        })*/
+
 
 
     }
     private fun initialiseSeekBar(){
         val seekbar = findViewById<SeekBar>(R.id.ske_activity_audio_player)
         seekbar.max = mp!!.duration
-        val handler = Handler()
+        //val handler = Handler()
+        val handler = Handler(Looper.getMainLooper())
         handler.postDelayed(object:Runnable{
             override fun run() {
                 try {
