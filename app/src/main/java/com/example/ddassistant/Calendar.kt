bcb07b207ceda07eef89e81f67c8e59117a12d09
agus <agustin.luvini@gmail.com>
2021-05-24 23:01:06 -0300

    Calendar

--- app/src/main/java/com/example/ddassistant/Calendar.kt
+++ app/src/main/java/com/example/ddassistant/Calendar.kt
@@ -1,19 +1,17 @@
 package com.example.ddassistant
 
-import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.provider.CalendarContract
 import android.provider.CalendarContract.Events
 import android.widget.Button
-import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 import java.util.Calendar
-import java.util.Calendar.getInstance
-import kotlin.properties.Delegates
 
 
 class Calendar : AppCompatActivity() {
@@ -35,7 +33,7 @@ class Calendar : AppCompatActivity() {
         val myyear = cal.get(Calendar.YEAR)
         val mymonth = cal.get(Calendar.MONTH)
         val day = cal.get(Calendar.DAY_OF_MONTH)
-        val dia = findViewById<TextView>(R.id.calendariodia)
+
 
 
         //var yearInt :Int
@@ -44,26 +42,33 @@ class Calendar : AppCompatActivity() {
 
         dateSelect.setOnClickListener {
             val datePickerDialog = DatePickerDialog(this,DatePickerDialog.OnDateSetListener {view, year, month, dayOfMonth ->
-                dateSelect.text= "Fecha del evento: "+ dayOfMonth + "/ " +(month+1) + "/ " + year
+                dateSelect.text= "Fecha: "+ dayOfMonth + "/ " +(month+1) + "/ " + year
                 yearInt = year
                 monthInt=month
                 dayInt= dayOfMonth
             },myyear,mymonth,day)
             datePickerDialog.show()
         }
-        dia.setOnClickListener {
-            dia.text = "$yearInt"
 
-        }
 
 
 
         addEventButton.setOnClickListener {
+            val titleText = findViewById<EditText>(R.id.txt_activity_calendar_title).text.toString()
+            val placeText = findViewById<EditText>(R.id.txt_activity_calendar_place).text.toString()
+            val descripText = findViewById<EditText>(R.id.txt_activity_calendar_description).text.toString()
             val calIntent = Intent(Intent.ACTION_INSERT)
+            val switchPrivate = findViewById<Switch>(R.id.switch_activity_calendar_private)
+
             calIntent.type = "vnd.android.cursor.item/event"
-            calIntent.putExtra(Events.TITLE, "My House Party")
-            calIntent.putExtra(Events.EVENT_LOCATION, "My Beach House")
-            calIntent.putExtra(Events.DESCRIPTION, "A Pig Roast on the Beach")
+            calIntent.putExtra(Events.TITLE, titleText)
+            calIntent.putExtra(Events.EVENT_LOCATION, placeText)
+            calIntent.putExtra(Events.DESCRIPTION, descripText)
+            if (switchPrivate.isChecked){ calIntent.putExtra(Events.ACCESS_LEVEL, Events.ACCESS_PRIVATE)
+            } else calIntent.putExtra(Events.ACCESS_LEVEL, Events.ACCESS_PUBLIC)
+
+            calIntent.putExtra(Events.RRULE,"FREQ=WEEKLY")
+
 
             val calDate = GregorianCalendar(yearInt, monthInt, dayInt)
             calIntent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true)
